bc0659e0fa8c3cadd80404067b93a8641cd2ab92
Dayhta <92410413+Dayhta@users.noreply.github.com>
2024-02-01 13:31:43 -0500

    Updated fixmyjson.py
    Added the main function to be added into overarching app

--- fixmyjson.py
+++ fixmyjson.py
@@ -1,8 +1,25 @@
 from fix_busted_json import repair_json
 
-def jsonData():
-    pass
+badJson = '/home/marriott-2023-12-19T18 27 33Z.json'
+outputJson = str(input("Please enter your new desired file name: ")) 
+
+def fixJsonFile(inputFile, outputFile):
+    try:
+        with open(inputFile, 'r') as file:
+            json = file.read()
+
+        fixedJson = repair_json(json)
+
+        with open(outputFile, 'w') as outputFile:
+            outputFile.write(fixedJson)
+
+        print(f"JSON properly formatted'{outputFile}'.")
+
+    except FileNotFoundError:
+        print(f"Error: File '{inputFile}' not found.")
+    except Exception as e:
+        print(f"[---] ERROR: \n\n{e}")
 
 
-def jsonFile():
-    pass 
+
+fixJsonFile(badJson, outputJson)
